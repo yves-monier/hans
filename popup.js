e79747ff0267a7e3cf169bb0e53b12a48e4bba7d
yves-monier <yves.monier@gmail.com>
2018-12-02 21:56:19 +0100

    noop

--- popup.js
+++ popup.js
@@ -164,6 +164,7 @@ function provideHelp() {
         queryArnastofnun(selectedText, true);
 
         // TODO
+        
         /*
         1. Get lemma(s) [bin.arnastofnun.is]
         2. For each lemma, get dictonary entries [digicoll.library.wisc.edu]
